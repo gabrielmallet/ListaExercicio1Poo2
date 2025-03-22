11e3d20c99e32ba88e586b300eec88363bb5a056
gabrielmallet <bielmallet@hotmail.com>
2025-03-21 21:25:13 -0300

    Commit 1

--- src/MultiplasThreads.java
+++ src/MultiplasThreads.java
@@ -1,13 +1,26 @@
-import javax.swing.*;
+public class MultiplasThreads extends Thread{
+    private int threadId;
 
-public class MultiplasThreads extends JFrame{
-
-    public MultiplasThreads(){
-        setSize(500,500);
+    public MultiplasThreads(int threadId) {
+        this.threadId = threadId;
     }
 
+    @Override
+    public void run() {
+    for (int i = 1; i <= 10; i++) {
+        System.out.println("Thread " + threadId + " - Contagem: " + i);
+            try {
+                Thread.sleep(500); // Pequeno atraso de 500ms
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-    public static void main(String[] args){
-        new MultiplasThreads();
+public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+    MultiplasThreads thread = new MultiplasThreads(i);
+    thread.start(); // Inicia a thread
+        }
     }
 }
